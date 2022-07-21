88b0daf61c677a71d4b3180cdc172013c249d60f
MariaSliufko <maria.sliufko@icloud.com>
2022-07-21 12:33:05 +0200

    fixe wordmatcher

--- WordUnscambler/Workers/FileReader.cs
+++ WordUnscambler/Workers/FileReader.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace WordUnscambler.Workers
 {
     public class FileReader
     {
-        
+        public string[] Read(string filename)
+        {
+            string[] fileContent;
+            try
+            {
+                fileContent = File.ReadAllLines(filename);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return fileContent;
+        }
     }
 }
